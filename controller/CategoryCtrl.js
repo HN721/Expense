690661ba341a5c693aad8e0ce659abdde46bf8a1
HN721 <119390246+HN721@users.noreply.github.com>
2024-11-03 22:54:36 +0700

    add create category

--- controller/CategoryCtrl.js
+++ controller/CategoryCtrl.js
@@ -1,9 +1,33 @@
 const User = require("../model/User");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+const Category = require("../model/Category");
 const categoryController = {
   create: async (req, res) => {
-    const { username, email, password } = req.body;
+    const { name, type } = req.body;
+    if (!name || !type) throw new Error("Name and type required");
+    const normalizeName = name.toLowerCase();
+    //chacek if type is valid
+    const validTypes = ["income", "expense"];
+    if (!validTypes.includes(type.toLowerCase())) {
+      throw new Error("Invalid category type" + type);
+    }
+    //check if category already exits on the user
+    const categoryExits = Category.findOne({
+      name: normalizeName,
+      user: req.user,
+    });
+    if (categoryExits) {
+      throw new Error(
+        `Category ${categoryExits.name} already exits in the database`
+      );
+    }
+    const category = await Category.create({
+      name: normalizeName,
+      user: req.user,
+      type,
+    });
+    res.status(201).json(category);
   },
   //login
   list: async (req, res) => {},
