9b6401db5d8d474755417a9e7492818ec249c092
HN721 <119390246+HN721@users.noreply.github.com>
2024-11-04 11:16:30 +0700

    add update category and delete

--- controller/CategoryCtrl.js
+++ controller/CategoryCtrl.js
@@ -1,4 +1,6 @@
 const Category = require("../model/Category");
+const Transaction = require("../model/Transaction");
+
 const categoryController = {
   create: async (req, res) => {
     const { name, type } = req.body;
@@ -32,9 +34,46 @@ const categoryController = {
     res.status(200).json(categories);
   },
 
-  //profile
-  update: async (req, res) => {},
-  delete: async (req, res) => {},
+  update: async (req, res) => {
+    const { categoryId } = req.params;
+    const { type, name } = req.body;
+    const normalizeName = name.toLowerCase();
+    const category = await Category.findById(categoryId);
+    if (!category && category.user.toString() !== req.user.toString()) {
+      throw new Error("category not found");
+    }
+
+    const oldName = category.name;
+    //update categories propertis
+    category.name = normalizeName || category.name;
+    category.type = type || category.type;
+    const updateCategory = await category.save();
+    //update affected transaksi
+    if (oldName !== updateCategory.name) {
+      await Transaction.updateMany(
+        {
+          user: req.user,
+          category: oldName,
+        },
+        { $set: { category: updateCategory.name } }
+      );
+    }
+    res.json(updateCategory);
+  },
+  delete: async (req, res) => {
+    const category = await Category.findById(req.params.id);
+    if (category && category.user.toString() === req.user.toString()) {
+      const defaultCategory = "Uncategorized";
+      await Transaction.updateMany(
+        { user: req.user, category: category._id },
+        { $set: { category: defaultCategory } }
+      );
+      await Category.findByIdAndDelete(req.params.id);
+      res.json({ message: "Category removed and transaksi updated" });
+    } else {
+      res.json({ message: "Categorynit founf" });
+    }
+  },
 };
 
 module.exports = categoryController;
--- routes/CategoryRoute.js
+++ routes/CategoryRoute.js
@@ -6,6 +6,16 @@ const router = express.Router();
 
 router.post("/categories/create", isAuthenticated, categoryController.create);
 router.get("/categories/lists", isAuthenticated, categoryController.list);
+router.put(
+  "/categories/update/:categoryId",
+  isAuthenticated,
+  categoryController.update
+);
+router.delete(
+  "/categories/delete/:id",
+  isAuthenticated,
+  categoryController.delete
+);
 
 // Add additional routes for login and profile when implemented in userController
 
